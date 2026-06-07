08ed4fa775258b41847883bcbe9739d93da68126
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-07 12:32:15 +0600

    constructor in py

--- OOPS_IN_PY/obj&cls01.py
+++ OOPS_IN_PY/obj&cls01.py
@@ -1,6 +1,10 @@
 class Student:
-    name = "Amit"
+    # name = "Amit"
+
+    def __init__(self, fullName):  # constructor
+        self.name = fullName
+        print("adding new student in DB")
 
 
-s1 = Student()
+s1 = Student("Amit")
 print(s1.name)
